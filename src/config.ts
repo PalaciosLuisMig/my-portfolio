15179048f4d4568027a0aebfc314265adc2b1dfe
PalaciosLuisMig <palaciosluismig@gmail.com>
2024-02-10 01:51:37 -0500

    Start of the project and a little more

---
+++ src/config.ts
@@ -0,0 +1,45 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// ---------- CV Download Link ----------
+export const CV_DOWNLOAD = "";
+
+// ---------- Menu Items ----------
+export const MENU_ITEMS = [
+  {
+    title: "Perfil",
+    tooltip: "bottom",
+    url: "/profile",
+    icon: "profile",
+  },
+  {
+    title: "Experiencia",
+    tooltip: "bottom",
+    url: "/experience",
+    icon: "experience",
+  },
+  {
+    title: "Proyectos",
+    tooltip: "bottom",
+    url: "/projects",
+    icon: "projects",
+  },
+  {
+    title: "Blog",
+    tooltip: "bottom",
+    url: "/blog",
+    icon: "blog",
+  },
+  {
+    title: "Contact",
+    tooltip: "right",
+    url: "/contact",
+    icon: "contact",
+  },
+];
+
+
+// ---------- Global Variables ----------
+export const TRANSITION_API = true;
+export const SITE_TITLE = "Portfolio de Luis Palacios ";
+export const SITE_DESCRIPTION = "Sitio web de mi portafolio personal creado con Astro y TailwindCSS.";
