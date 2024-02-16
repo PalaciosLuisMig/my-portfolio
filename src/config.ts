25e5b88767ddad502d52a515ad7a0226bc402775
PalaciosLuisMig <palaciosluismig@gmail.com>
2024-02-16 00:57:38 -0500

    Terminar sección de tecnologías, agregar la sección de educación y contacto :)

--- src/config.ts
+++ src/config.ts
@@ -90,6 +90,113 @@ export const TECH_STACK = [
     logo: "tailwindcss",
     name: "Tailwind",
   },
+  {
+    category: "Back-end",
+    logo: "java",
+    name: "Java",
+  },
+  {
+    category: "Back-end",
+    logo: "python",
+    name: "Python",
+  },
+  {
+    category: "Back-end",
+    logo: "csharp",
+    name: "C#",
+  },
+  {
+    category: "Back-end",
+    logo: "quarkus",
+    name: "Quarkus",
+  },
+  {
+    category: "Back-end",
+    logo: "spring",
+    name: "Spring Boot",
+  },
+  {
+    category: "Back-end",
+    logo: "docker",
+    name: "Docker",
+  },
+  {
+    category: "Back-end",
+    logo: "jmeter",
+    name: "JMeter",
+  },
+  {
+    category: "Database",
+    logo: "sqlserver",
+    name: "SQL Server",
+  },
+  {
+    category: "Database",
+    logo: "mysql",
+    name: "MySQL",
+  },
+  {
+    category: "Database",
+    logo: "postgresql",
+    name: "PosrgresQL",
+  },
+  {
+    category: "Database",
+    logo: "sqlite",
+    name: "SQLite",
+  },
+  {
+    category: "Database",
+    logo: "mongodb",
+    name: "MongoDB",
+  },
+  {
+    category: "Version-Control",
+    logo: "git",
+    name: "Git",
+  },
+  {
+    category: "Version-Control",
+    logo: "gitlab",
+    name: "GitLab",
+  },
+  {
+    category: "Version-Control",
+    logo: "github",
+    name: "GitHub",
+  },
+  {
+    category: "Low-Code",
+    logo: "outsystems",
+    name: "OutSystems",
+  },
+];
+
+export const EDUCATIONS = [
+  {
+    educational_center: "Universidad Católica de Cuenca",
+    specialty: "Ingeniería de Sistemas",
+    date: "Ago 2016 - Feb 2020",
+    image: "ucacue.png",
+  },
+  {
+    educational_center: "Universidad Internacional de la Rioja",
+    specialty: "Maestría en Ingeniería de Software y Sistemas Informáticos",
+    date: "Oct 2022 - Jul 2023",
+    image: "unir.webp",
+  },
+  {
+    educational_center: "CertiProf",
+    specialty: "SDPC - Scrum Developer Professional Certificate",
+    date: "Ene 2021",
+    image: "certiprof.webp",
+  },
+  {
+    educational_center: "CertiProf",
+    specialty: "SMPC - Scrum Master Professional Certificate",
+    date: "Jul 2020",
+    image: "certiprof.webp",
+  },
 ];
 
 // ---------- Global Variables ----------
