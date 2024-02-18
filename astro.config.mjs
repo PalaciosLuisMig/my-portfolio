1e2494b6a71b6026a83c89760a7d6d3c7e52f156
PalaciosLuisMig <palaciosluismig@gmail.com>
2024-02-17 22:08:57 -0500

    Agregar el footer y integración con netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  output: "server",
+  adapter: netlify()
 });
