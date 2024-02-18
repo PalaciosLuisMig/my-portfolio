e4feb1e27d84a2bf75c54c3d94095a5d6d24823a
PalaciosLuisMig <palaciosluismig@gmail.com>
2024-02-17 20:13:03 -0500

    Agregar funcionalidad el boton enviar y copiar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap(), icon()]
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
